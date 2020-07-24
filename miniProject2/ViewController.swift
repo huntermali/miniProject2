75ea5779ea944004d3ae9549ac11c39649855a69
Hunter Malinowski <huntermalinowski@gmail.com>
2020-07-24 07:07:07 -0700

    Initial Commit

---
+++ miniProject2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  miniProject2
+//
+//  Created by Apple on 7/24/20.
+//  Copyright © 2020 Hunter Malinowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
